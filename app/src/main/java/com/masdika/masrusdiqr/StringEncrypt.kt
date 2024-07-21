cacceafa5b49ac3d4bad891b90d3cb17a5f1abad
Masdikaa <masdikailhanmansiz@gmail.com>
2024-07-21 09:19:20 +0700

    Password encryption

---
+++ app/src/main/java/com/masdika/masrusdiqr/StringEncrypt.kt
@@ -0,0 +1,14 @@
+package com.masdika.masrusdiqr
+
+class StringEncrypt {
+
+    private val key: String = ""
+    private val iv: String = ""
+    private val secretKeySpec = null
+    private val ivParameterSpec = null
+
+    fun encrypt(password: String): String {
+        val encryptedString = ""
+        return encryptedString
+    }
+}
--- app/src/main/java/com/masdika/masrusdiqr/fragments/LoginFragment.kt
+++ app/src/main/java/com/masdika/masrusdiqr/fragments/LoginFragment.kt
@@ -3,16 +3,20 @@ package com.masdika.masrusdiqr.fragments
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.masdika.masrusdiqr.StringEncrypt
 import com.masdika.masrusdiqr.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private val encryptPassword = StringEncrypt()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +30,30 @@ class LoginFragment : Fragment() {
         binding.signUpButton.text = spannableString
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.loginButton.setOnClickListener {
+            val username = binding.inputUsername.text.toString()
+            val password = binding.inputPassword.text.toString()
+            val passwordHash = encryptPassword.encrypt(password)
+
+//            if (username.isEmpty()) {
+//                binding.inputUsername.error = "Enter your username"
+//                binding.inputUsername.requestFocus()
+//            } else if (password.isEmpty()) {
+//                binding.inputUsername.error = "Password must not be empty"
+//                binding.inputUsername.requestFocus()
+//            } else if (password.length < 8) {
+//                binding.inputUsername.error = "Password must not be less than 8 characters"
+//                binding.inputUsername.requestFocus()
+//            }
+
+            Log.d("Password Hash", passwordHash)
+            Log.d("Password", password)
+        }
     }
 
 }
